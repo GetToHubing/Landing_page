9db03813f2962b8ab7c87d7c2926db72ba9ebe4d
David <davidbanker22@yahoo.com>
2023-07-21 23:19:01 -0500

    the users order is loaded into the checkout view. need to add styling and the correct amount for the order

--- src/main/java/controllers/PizzaProjectControllers/CheckoutServlet.java
+++ src/main/java/controllers/PizzaProjectControllers/CheckoutServlet.java
@@ -21,26 +21,26 @@ public class CheckoutServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Map<String, String[]> newPizza = request.getParameterMap(); //takes the parameters from the pizza order form and makes a map
         Map<String, Map<String, String[]>> newOrder; //a new map that holds all the pizzas a user makes
-        int counter = 0;
+        int counter = 1;
         if (request.getSession().getAttribute("order") == null) { //make new hashmap if there's no order attribute and add the first pizza to hashmap
             newOrder = new HashMap<>();
-            newOrder.put("item" + counter, newPizza);
+            newOrder.put("item " + counter, newPizza);
         } else { //add pizzas to the order attribute if there's already one
             newOrder = (Map<String, Map<String, String[]>>) request.getSession().getAttribute("order");
             counter++;
-            newOrder.put("item" + counter, newPizza);
-            System.out.println(newOrder.size());
+            newOrder.put("item " + counter, newPizza);
+//            System.out.println(newOrder.entrySet().size());
         }
-        request.getSession().setAttribute("order", newOrder); //sets the new hashmap to a session attribute named order
+        request.getSession().setAttribute("order", newOrder); //sets the new/updated hashmap to a session attribute named order
         //the above code is for users to add multiple items to one order
-        for (Map.Entry<String, Map<String, String[]>> i : newOrder.entrySet()) {
-            System.out.println(i.getKey());
-            for (Map.Entry<String, String[]> j : i.getValue().entrySet()) {
-                System.out.println(j.getKey() + ": " + Arrays.toString(j.getValue()));
-            }
-            System.out.println("--------------");
-        }
-        response.sendRedirect("checkout");
+//        for (Map.Entry<String, Map<String, String[]>> i : newOrder.entrySet()) {
+//            System.out.println(i.getKey());
+//            for (Map.Entry<String, String[]> j : i.getValue().entrySet()) {
+//                System.out.println(j.getKey() + ": " + Arrays.toString(j.getValue()));
+//            }
+//            System.out.println("--------------");
+//        }
+        request.getRequestDispatcher("WEB-INF/checkout.jsp").forward(request, response);
 
     }
 }
--- src/main/webapp/WEB-INF/checkout.jsp
+++ src/main/webapp/WEB-INF/checkout.jsp
@@ -1,10 +1,13 @@
-<%--
+<%@ page import="java.util.Map" %>
+<%@ page import="java.util.Arrays" %><%--
   Created by IntelliJ IDEA.
   User: davidbanker
   Date: 7/19/23
   Time: 4:28 PM
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!doctype html>
 
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -18,12 +21,28 @@
     <title>Checkout</title>
 </head>
 <body>
-<p>hello</p>
-<%--    <form action="pizzaOrder" method="post">--%>
+<main>
+    <% Map<String, Map<String, String[]>> newOrder = (Map<String, Map<String, String[]>>) request.getSession().getAttribute("order"); //sets the new/updated hashmap to a session attribute named order
+        //the above code is for users to add multiple items to one order
+        for (Map.Entry<String, Map<String, String[]>> i : newOrder.entrySet()) {
+            System.out.println(i.getKey());
+            for (Map.Entry<String, String[]> j : i.getValue().entrySet()) {
+                System.out.println(j.getKey() + ": " + Arrays.toString(j.getValue()));
+            }
+            System.out.println("--------------");
+        } %>
+    <c:forEach items="${sessionScope.order}" var="entry">
+        <p>${entry.key}</p>
+        <c:forEach items="${entry.value.entrySet()}" var="pizzaEntry">
+            <p>${pizzaEntry.key}: ${pizzaEntry.value[0]}</p>
+            <p></p>
+        </c:forEach>
+        <hr/>
+    </c:forEach>
+
     <a href="pizzaOrder">
         <button type="submit" class="btn btn-primary">Add Item</button>
     </a>
-<%--    </form>--%>
-
+</main>
 </body>
 </html>
